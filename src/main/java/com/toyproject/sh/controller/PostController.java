ad2ad0033fd4982600baa73eeed947b3e001fec0
손상욱 <thstkdnr20@naver.com>
2024-01-29 22:37:19 +0900

    createPost 생성완료

--- src/main/java/com/toyproject/sh/controller/PostController.java
+++ src/main/java/com/toyproject/sh/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.toyproject.sh.controller;
 
+import com.toyproject.sh.domain.Member;
+import com.toyproject.sh.domain.Post;
+import com.toyproject.sh.dto.CreatePostRequest;
 import com.toyproject.sh.service.PostService;
+import com.toyproject.sh.session.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,6 +19,15 @@ public class PostController {
 
     private final PostService postService;
 
+    @PostMapping("/new") //TODO 태그 오류 핸들링
+    public String createPost(@RequestBody CreatePostRequest postRequest,
+                             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember) {
+        if (loginMember == null) {
+            return "redirect:/members/login";
+        }
 
-
+        Post post = new Post(loginMember, postRequest.getThumbnail(), postRequest.getContent(), postRequest.getCategory());
+        postService.createPost(post, postRequest.getTagName());
+        return "/";
+    }
 }
