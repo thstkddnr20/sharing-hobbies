d03eae74922f7681b7b474cbaa4044150c0d7dfd
손상욱 <thstkdnr20@naver.com>
2024-01-29 22:37:42 +0900

    Category enum타입을 json형태로 받기 위해 수정완료

--- src/main/java/com/toyproject/sh/domain/Category.java
+++ src/main/java/com/toyproject/sh/domain/Category.java
@@ -1,10 +1,36 @@
 package com.toyproject.sh.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
 public enum Category {
-    INTRODUCE,
-    GUIDE,
-    SPECIALHOBBIES,
-    TOGETHER,
-    FREE,
-    QUESTION
+    INTRODUCE("int"),
+    GUIDE("gui"),
+    SPECIALHOBBIES("spe"),
+    TOGETHER("tog"),
+    FREE("free"),
+    QUESTION("qna");
+
+    @Getter
+    private final String value;
+
+    Category(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static Category from(String value) {
+        for (Category status : Category.values()) {
+            if (status.getValue().equals(value)) {
+                return status;
+            }
+        }
+        return null;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
