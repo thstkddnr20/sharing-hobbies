0caae70d55821a976116d3dda49a68aa4f6f878c
thstkddnr20 <thstkddnr20@naver.com>
2024-02-08 15:04:03 +0900

    일반 home과 로그인된 loginHome을 HomeController를 사용하여 분리하였고 유저 기본적인 회원가입, 로그인, 로그아웃을 구현하였다.

--- src/main/java/com/toyproject/sh/controller/MemberController.java
+++ src/main/java/com/toyproject/sh/controller/MemberController.java
@@ -4,12 +4,15 @@ import com.toyproject.sh.domain.Member;
 import com.toyproject.sh.dto.MemberRequest;
 import com.toyproject.sh.exception.ExceptionHandler;
 import com.toyproject.sh.service.MemberService;
+import com.toyproject.sh.session.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,11 +45,52 @@ public class MemberController {
         try {
             Member member = new Member(request.getEmail(), passwordEncoder.encode(request.getPassword()));
             Long id = memberService.join(member);
-            return "redirect:/index";
+            return "redirect:/";
         } catch (ExceptionHandler e) {
             bindingResult.rejectValue("email", "Duplicate Email", e.getMessage());
             log.error("회원가입 오류={}", e.getMessage());
             return "members/register";
         }
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(MemberRequest memberRequest) { return "members/login"; }
+
+    @PostMapping("/login")
+    public String login(@Valid MemberRequest memberRequest, BindingResult bindingResult, HttpServletRequest request){
+
+        if (bindingResult.hasErrors()){
+            StringBuilder errorMessage = new StringBuilder("Validation failed for the following fields: ");
+            bindingResult.getFieldErrors().forEach(error ->
+                    errorMessage.append(error.getField()).append(" - ").append(error.getDefaultMessage())
+            );
+            log.error("로그인 오류={}", errorMessage);
+            return "members/login";
+        }
+
+        Member member = memberService.findMember(memberRequest.getEmail());
+
+        if (member == null || !passwordEncoder.matches(memberRequest.getPassword(), member.getPassword())) {
+            bindingResult.rejectValue("email", "Duplicate Email", "아이디 또는 비밀번호가 잘못되었습니다.");
+            log.error("로그인 정보 입력 오류");
+            return "members/login";
+        }
+        else {
+            //getSession 세션이 있는경우 세션반환, 없을경우 새로운 세션 생성
+            HttpSession session = request.getSession();
+            //세션에 로그인 회원 정보 저장
+            session.setAttribute(SessionConst.LOGIN_MEMBER, member);
+
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
     }
 }
